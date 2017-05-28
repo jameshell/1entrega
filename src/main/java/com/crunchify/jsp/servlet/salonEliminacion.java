9cfc8ac99409abc3cafcc428e51ade95314acdc2
james <james@JamesNewHell>
2017-05-28 07:31:53 -0500

    Actualizando servlets

---
+++ src/main/java/com/crunchify/jsp/servlet/salonEliminacion.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crunchify.jsp.servlet;
+
+import edu.co.sergio.mundo.dao.SalonDAO;
+import edu.co.sergio.mundo.dao.exceptions.IllegalOrphanException;
+import edu.co.sergio.mundo.dao.exceptions.NonexistentEntityException;
+import edu.co.sergio.mundo.vo.Persona;
+import edu.co.sergio.mundo.vo.Salon;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+ 
+/**
+ * @author Crunchify.com
+ */
+ 
+public class salonEliminacion extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // reading the user input
+        String id = request.getParameter("idSalon");
+     
+        
+        //Se debe incluir validaciones - Lo recuerda: Gestion de Excepciones.
+        SalonDAO dao = new SalonDAO();
+        Salon salon = new Salon();
+        salon.setIdsalon(Integer.parseInt(id));
+      
+        try {
+            dao.destroy(Integer.parseInt(id));
+        } catch (IllegalOrphanException ex) {
+            Logger.getLogger(personaEliminacion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(personaEliminacion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+      
+       
+        //Redireccionando la informacion
+        RequestDispatcher redireccion = request.getRequestDispatcher("salon.jsp");
+        redireccion.forward(request, response);
+        
+        
+        }
+}
