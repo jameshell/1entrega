6d12980e0e74e0ebdffe4dd942ee0644d8d08b6a
james <james@JamesNewHell>
2017-05-30 08:10:31 -0500

    Servlet de prestamoSalon

--- src/main/java/com/crunchify/jsp/servlet/listaControladorMantenimiento.java
+++ src/main/java/com/crunchify/jsp/servlet/listaControladorMantenimiento.java
@@ -1,16 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.crunchify.jsp.servlet;
+ 
 
+import edu.co.sergio.mundo.dao.AdministrativoDAO;
+import edu.co.sergio.mundo.dao.MantenimientoDAO;
+import edu.co.sergio.mundo.vo.Administrativo;
+import edu.co.sergio.mundo.vo.Mantenimiento;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+ 
 /**
- *
- * @author james
+ * @author Crunchify.com
  */
-public class listaControladorMantenimiento {
-    
-    
-    
+ 
+public class listaControladorMantenimiento extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //Se debe incluir validaciones - Lo recuerda: Gestion de Excepciones.
+        MantenimientoDAO dao = new MantenimientoDAO();
+
+        
+        //Listando la informacion  
+        List<Mantenimiento> mants =  dao.findMantenimientoEntities();
+        request.setAttribute("mants", mants);
+       
+       
+        //Redireccionando la informacion
+        RequestDispatcher redireccion = request.getRequestDispatcher("listaMantenimiento.jsp");
+        redireccion.forward(request, response);
+        
+        
+        }
 }
